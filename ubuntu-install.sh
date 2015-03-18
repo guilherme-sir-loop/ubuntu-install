7271d1350ba4c3690269d6d339f4740dd0bcdb14
guilherme-sir-loop <guilhermeaugustopc@gmail.com>
2015-03-18 15:32:17 -0300

    shell script
    add phonegap

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -9,14 +9,26 @@ alias ins-repo="sudo apt-get install"
 #Install the Java Enviroment
 add-repo ppa:webupd8team/java
 upd-repo
-ins-repo -y oracle-java7-installer
-ins-repo -y oracle-java7-set-default
+ins -y oracle-java7-installer
+ins -y oracle-java7-set-default
 #Ant
 ins-repo -y ant
 # ANDROID ADB
 add-repo ppa:nilarimogard/webupd8
 upd-repo
-ins-repo -y android-tools-adb android-tools-fastboot
-ins-repo -y  libc6:i386 libgcc1:i386 gcc-4.6-base:i386 libstdc++5:i386 libstdc++6:i386
+ins -y android-tools-adb android-tools-fastboot
+ins -y  libc6:i386 libgcc1:i386 gcc-4.6-base:i386 libstdc++5:i386 libstdc++6:i386
 echo "export PATH=${PATH}:$HOME/android-sdk/sdk/platform-tools" >> .bashrc
 echo "export PATH=${PATH}:$HOME/android-sdk/sdk/tools" >> .bashrc
+echo $PATH
+android-sdk
+#PhoneGAP
+install -y python-software-properties python g++ make
+add-repo ppa:chris-lea/node.js 
+upd-repo
+install -y nodejs
+node -v
+sudo npm install -g cordova
+cd android-sdk
+mkdir cordova
+cd cordova
