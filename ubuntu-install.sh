bac39aaf3341e062646723a274fd7e9467d06b4e
guilherme-sir-loop <guilhermeaugustopc@gmail.com>
2016-05-17 11:28:55 -0300

    Changed Description and Xubuntu improvements
    Now i just want to xfce, not openbox. And installed  synapse.

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 # @author:Guilherme Augusto Peixoto de Campos
 # @date:18/03/2015
-# @description:A Script to install the basic applications for my ubuntu usage, when i am using openbox
+# @description:A Script to install the basic applications for my ubuntu usage, when i am xubuntu
 
 
 #Alias
-alias add-repo="sudo add-apt-repository"
-alias upd-repo="sudo apt-get update"
-alias ins-repo="sudo apt-get install"
+alias add_repo="sudo add-apt-repository"
+alias upd_repo="sudo apt-get update"
+alias ins="sudo apt-get install"
 
 #Install the Java Enviroment
 function Java_Enviroment(){
-	add-repo ppa:webupd8team/java
-    upd-repo
+    add_repo ppa:webupd8team/java
+    upd_repo
     ins -y oracle-java7-installer
     ins -y oracle-java7-set-default
     #Ant
-    ins-repo -y ant
+    ins -y ant
     #eclipse
-    install -y eclipse-plataform
+    ins -y eclipse-plataform
 }
 
 function android_development(){
 	# ANDROID ADB
-    add-repo ppa:nilarimogard/webupd8
-    upd-repo
+    add_repo ppa:nilarimogard/webupd8
+    upd_repo
     ins -y android-tools-adb android-tools-fastboot
     ins -y  libc6:i386 libgcc1:i386 gcc-4.6-base:i386 libstdc++5:i386 libstdc++6:i386
     echo "export PATH=${PATH}:$HOME/android-sdk/sdk/platform-tools" >> .bashrc
@@ -35,10 +35,10 @@ function android_development(){
 
 function phonegap(){
 	#PhoneGAP
-    install -y python-software-properties python g++ make
+    ins -y python-software-properties python g++ make
     add-repo ppa:chris-lea/node.js 
     upd-repo
-    install -y nodejs
+    ins -y nodejs
     node -v
     sudo npm install -g cordova
     cd android-sdk
@@ -49,16 +49,16 @@ function phonegap(){
 
 function sublime(){
 	#Sublime Text 3
-    add-repo ppa:webupd8team/sublime-text-3
-    upd-repo
-    install -y sublime-text-installer
+    add_repo ppa:webupd8team/sublime-text-3
+    upd_repo
+    ins -y sublime-text-installer
 }
 
 function git(){
 	#SmartGit,i am a noob a need GUI tools
     wget -c http://www.syntevo.com/smartgit/download?file=smartgit/smartgit-6_5_6.deb
     dpkg -i smartgit-6_5_6.deb
-    install -y git
+    ins -y git
 }
 function web(){
 	#google chrome
@@ -71,6 +71,9 @@ function xubuntu_solve_lock_error(){
 sudo mkdir /etc/X11/xorg.conf.d/
 echo -e 'Section "Device"\n Identifier "Card0"\n Driver "Intel"\n Option "AccelMethod" "uxa"\nEndSection' | sudo tee /etc/X11/xorg.conf.d/20-intel.conf
 }
+function synapse(){
+	ins -y synapse 
+}
 
 
 android_development()
@@ -80,3 +83,4 @@ git()
 web()
 xubuntu_solve_lock_error()
 Java_Enviroment()
+synapse()
